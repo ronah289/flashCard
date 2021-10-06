ea31840c82dd19ae50dd10fc1dd6c85304661970
ronah289 <ronald.kibet@student.moringaschool.com>
2021-10-06 12:17:29 -0230

    add cards

--- src/app/card/card.component.ts
+++ src/app/card/card.component.ts
@@ -10,6 +10,8 @@ export class CardComponent implements OnInit {
   Cards: Card[] = [
     new Card(1,'card1','card 1 description'),
     new Card(2,'card2','card2 description'),
+    new Card(3,'card3','card 3 description'),
+    new Card(4,'card4','card 4 description'),
   ];
   showD(index:any){
     this.Cards[index].showD = !this.Cards[index].showD;
